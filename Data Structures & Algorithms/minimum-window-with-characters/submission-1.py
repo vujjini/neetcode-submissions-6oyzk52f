512624d6619f441f4e6cfa9b8e5332f05f3e4fa2
sriram-hub007 <vujjiniram143@gmail.com>
2026-04-03 00:38:43 -0400

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        # keep track of a dictionary consisting of letters in t
+        # window min length == len(t)
+        # keep on adding letters to the window's dict until it matches t's dict
+        # once it does, increase the left pointer while the dict still matches
+        # update the res with the curr window length
+        # and keep it incrementing right pointer
+
+        def match(currWindow, t_count):
+            for char in t_count:
+                if currWindow[char] < t_count[char]:
+                    return False
+            return True
+
+        if len(t) > len(s):
+            return ""
+
+        res = ""
+        
+        t_count = Counter(t)
+        currWindow = {}
+        
+        for char in t:
+            currWindow[char] = 0
+        
+        l, r = 0, 0
+
+        while r < len(s):
+            if s[r] in currWindow:
+                currWindow[s[r]]+=1
+            print(currWindow)
+            while match(currWindow, t_count) and l <= r:
+                if len(res) == 0 or r - l + 1 <= len(res):
+                    res = s[l:r + 1]
+                if s[l] in currWindow:
+                    currWindow[s[l]]-=1
+                l+=1
+            r+=1
+        
+        return res
+            
